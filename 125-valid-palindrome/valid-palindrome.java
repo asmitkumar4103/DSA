f95af700497f6cc4fecd23e5d77f38aeec5778e4
Asmit Kumar <asmit4103@gmail.com>
2026-01-23 08:46:02 +0530

    Time: 4 ms (51.51%) | Memory: 45 MB (46.57%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+       
+        String str = sb.toString();
+        int n = str.length();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
